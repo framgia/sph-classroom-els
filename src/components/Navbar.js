d94a35f5ebfd3ec3ee4915e063d12122ec366527
John Harvey <89382692+JHarbee@users.noreply.github.com>
2021-10-28 18:12:30 +0800

    E_CLASSROOM-122 [Markup] Navigation Bar

---
+++ src/components/Navbar.js
@@ -0,0 +1,31 @@
+import React from 'react'
+import Navbar from 'react-bootstrap/Navbar'
+import { Container, Nav, NavDropdown, NavDropdowncolor } from 'react-bootstrap';
+
+
+function Navigationbar() {
+    return (
+        <div>
+            <Navbar className='navbar' expand="lg">
+                <Container fluid>
+                    <Navbar.Brand id="title">E-Learning</Navbar.Brand>
+                    <Nav
+                        style={{ maxHeight: '100px' }}
+                        navbarScroll
+                    >
+                        <Nav.Link href="#dashboard" id="Nav">Dashboard</Nav.Link>
+                        <Nav.Link href="#categories" id="Nav">Categories</Nav.Link>
+                        <Nav.Link href="#students" id="Nav">Students</Nav.Link>
+                        <NavDropdown title={<img src="https://www.freeiconspng.com/thumbs/profile-icon-png/account-profile-user-icon--icon-search-engine-10.png" className="p-icon" />} id="navbarScrollingDropdown">
+                            <NavDropdown.Item href="#learnings">Learnings</NavDropdown.Item>
+                            <NavDropdown.Item href="#profile">Profile</NavDropdown.Item>
+                            <NavDropdown.Item href="#login">Logout</NavDropdown.Item>
+                        </NavDropdown>
+                    </Nav>
+                </Container>
+            </Navbar >
+        </div >
+    );
+}
+
+export default Navigationbar;
