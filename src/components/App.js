e482cc961c1819260206093cfbaee821b7d23cb0
John Harvey <89382692+JHarbee@users.noreply.github.com>
2021-10-28 18:12:30 +0800

    E_CLASSROOM-122 [Markup] Navigation Bar

---
+++ src/components/App.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import Navbar from './Navbar';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './css/GlobalStyle.css'
+
+function App() {
+
+  return <Navbar />
+}
+
+export default App;
