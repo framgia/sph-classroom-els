0ddf05f9afbaf8d539abf99194ba1d98308e6df6
John Harvey <89382692+JHarbee@users.noreply.github.com>
2021-10-29 10:15:38 +0800

    Updated Dropdown

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import Navbar from 'react-bootstrap/Navbar'
 import { Container, Nav, NavDropdown, NavDropdowncolor } from 'react-bootstrap';
-
+import { ButtonGroup, Dropdown, DropdownButton } from 'react-bootstrap';
 
 function Navigationbar() {
     return (
@@ -16,11 +16,13 @@ function Navigationbar() {
                         <Nav.Link href="#dashboard" id="Nav">Dashboard</Nav.Link>
                         <Nav.Link href="#categories" id="Nav">Categories</Nav.Link>
                         <Nav.Link href="#students" id="Nav">Students</Nav.Link>
-                        <NavDropdown title={<img src="https://www.freeiconspng.com/thumbs/profile-icon-png/account-profile-user-icon--icon-search-engine-10.png" className="p-icon" />} id="navbarScrollingDropdown">
-                            <NavDropdown.Item href="#learnings">Learnings</NavDropdown.Item>
-                            <NavDropdown.Item href="#profile">Profile</NavDropdown.Item>
-                            <NavDropdown.Item href="#login">Logout</NavDropdown.Item>
-                        </NavDropdown>
+                        <Navbar.Collapse id="navbarScrollingDropdown">
+                            <NavDropdown align="end" title={<img src="https://www.freeiconspng.com/thumbs/profile-icon-png/account-profile-user-icon--icon-search-engine-10.png" className="p-icon" />} id="navbarScrollingDropdown">
+                                <NavDropdown.Item href="#learnings">Learnings</NavDropdown.Item>
+                                <NavDropdown.Item href="#profile">Profile</NavDropdown.Item>
+                                <NavDropdown.Item href="#login">Logout</NavDropdown.Item>
+                            </NavDropdown>
+                        </Navbar.Collapse>
                     </Nav>
                 </Container>
             </Navbar >
