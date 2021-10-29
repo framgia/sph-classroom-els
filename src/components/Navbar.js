bc0a0a5684612d323bed681f7becd02353ffd3f7
John Harvey <89382692+JHarbee@users.noreply.github.com>
2021-10-29 17:40:25 +0800

    Updated Navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,7 +1,9 @@
 import React from 'react'
 import Navbar from 'react-bootstrap/Navbar'
-import { Container, Nav, NavDropdown, NavDropdowncolor } from 'react-bootstrap';
-import { ButtonGroup, Dropdown, DropdownButton } from 'react-bootstrap';
+import { Container, Nav, NavDropdown, Col, Image, Dropdown } from 'react-bootstrap';
+import { IoLibraryOutline } from 'react-icons/io5';
+import { CgProfile } from 'react-icons/cg';
+import { BiLogOutCircle } from 'react-icons/bi';
 
 function Navigationbar() {
     return (
@@ -17,10 +19,10 @@ function Navigationbar() {
                         <Nav.Link href="#categories" id="Nav">Categories</Nav.Link>
                         <Nav.Link href="#students" id="Nav">Students</Nav.Link>
                         <Navbar.Collapse id="navbarScrollingDropdown">
-                            <NavDropdown align="end" title={<img src="https://www.freeiconspng.com/thumbs/profile-icon-png/account-profile-user-icon--icon-search-engine-10.png" className="p-icon" />} id="navbarScrollingDropdown">
-                                <NavDropdown.Item href="#learnings">Learnings</NavDropdown.Item>
-                                <NavDropdown.Item href="#profile">Profile</NavDropdown.Item>
-                                <NavDropdown.Item href="#login">Logout</NavDropdown.Item>
+                            <NavDropdown title={<img src="https://www.freeiconspng.com/thumbs/profile-icon-png/account-profile-user-icon--icon-search-engine-10.png" className="p-icon" />} id="navbarScrollingDropdown" autoClose="outside">
+                                <NavDropdown.Item href="" ><IoLibraryOutline /> Learnings</NavDropdown.Item>
+                                <NavDropdown.Item href=""><CgProfile /> Profile</NavDropdown.Item>
+                                <NavDropdown.Item href=""><BiLogOutCircle /> Logout</NavDropdown.Item>
                             </NavDropdown>
                         </Navbar.Collapse>
                     </Nav>
