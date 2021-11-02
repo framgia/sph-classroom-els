a0c47baf67bb93f8eeaf17d08215b351467b8c46
ErickDoroy <89514595+ErickDoroy@users.noreply.github.com>
2021-11-02 13:05:10 +0800

    E_CLASSROOM-42 [Frontend] [Student] [Markup] User Authentication 1/2

--- src/components/App.js
+++ src/components/App.js
@@ -2,10 +2,14 @@ import React from 'react'
 import Navbar from './Navbar';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/GlobalStyle.css'
+import Login from './users/Login';
 
 function App() {
 
-  return <Navbar />
+  return (
+    <Navbar />,
+    <Login/>
+ );
 }
 
 export default App;
