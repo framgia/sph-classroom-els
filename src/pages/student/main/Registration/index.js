4ca0f5c79e13484a5a74025b3711396bf3e96949
ErickDoroy <89514595+ErickDoroy@users.noreply.github.com>
2021-11-03 17:24:50 +0800

    E_CLASSROOM-43 [Frontend] [Student] [Markup] User Authentication 2/2 (#14)
    * E_CLASSROOM-43 [Frontend] [Student] [Markup] User Authentication 2/2
    
    * E_CLASSROOM-43 [Frontend] [Student] [Markup] User Authentication 2/2
    
    * E_CLASSROOM-43 [Frontend] [Student] [Markup] User Authentication 2/2

--- src/pages/student/main/Registration/index.js
+++ src/pages/student/main/Registration/index.js
@@ -1,7 +1,75 @@
 import React from 'react';
+import Button from 'react-bootstrap/Button';
+import Container from 'react-bootstrap/Container';
+import Stack from 'react-bootstrap/Stack';
+import Form from 'react-bootstrap/Form';
+import Navbar from 'react-bootstrap/Navbar';
+import style from './index.module.css';
 
 const Registration = () => {
-  return <div>Registration</div>;
+  return (
+    <div>
+      <div>
+        <div>
+          <Navbar id={style.navbar} expand="lg">
+            <Container fluid>
+              <Navbar.Brand id={style.title}>E-Learning</Navbar.Brand>
+            </Container>
+          </Navbar>
+        </div>
+      </div>
+      <Container style={{marginTop:'132px'}}>
+          <Stack gap={2} className="col-md-5 mx-auto" id={style.log01}>
+              <Form>
+                  <div>
+                      <Form.Group className="mb-3" controlId="CurrentPassword">
+                          <Form.Label><h5>Name</h5></Form.Label>
+                          <Form.Control  className="cntrs" type="password" placeholder="Input Username" />
+                          <Form.Text className="text-muted">
+                          </Form.Text>
+                      </Form.Group>
+
+                      <Form.Group className="mb-3" controlId="CurrentPassword">
+                          <Form.Label><h5>Email</h5></Form.Label>
+                          <Form.Control  className="cntrs" type="password" placeholder="Input Username" />
+                          <Form.Text className="text-muted">
+                          </Form.Text>
+                      </Form.Group>
+
+                      <Form.Group className="mb-3" controlId="NewPassword">
+                          <Form.Label><h5>Password</h5></Form.Label>
+                          <Form.Control  className="cntrs" type="password" placeholder="************" />
+                          <Form.Text className="text-muted">
+                          </Form.Text>
+                      </Form.Group>
+
+                      <Form.Group className="mb-3" controlId="NewPassword">
+                          <Form.Label><h5>Confirm Password</h5></Form.Label>
+                          <Form.Control  className="cntrs" type="password" placeholder="************" />
+                          <Form.Text className="text-muted">
+                          </Form.Text>
+                      </Form.Group>
+                      
+                      <center style={{marginTop:'65px'}}>
+                          <Button className={style.Btncolor} type="submit">
+                          <h5>Sign Up</h5>
+                          </Button>
+                      </center>
+                      
+                      <center>
+                          <div className="cnb">
+                          <p className={style.sign}>Already have an Account?</p>
+                          <h5 className={style.sign}>
+                              <a style={{textDecoration:'none'}} href="">Sign In</a>
+                          </h5>
+                          </div>
+                      </center>
+                  </div>
+              </Form>
+          </Stack>
+      </Container >
+    </div>
+  );
 };
 
 export default Registration;
