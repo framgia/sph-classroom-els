696b37c9a1950dff5a9ddd9e593307377cdd2fb0
John Paul Lim <89845252+Johnpaullim@users.noreply.github.com>
2021-11-03 17:37:44 +0800

    E_CLASSROOM-41 [Markup] [Student] Categories (#9)
    * E_CLASSROOM-41 [Markup] [Student] Categories
    
    * added padding top to adjust boxes
    
    * fix style UI comment
    
    * change header color
    
    * adjust title

--- src/pages/student/categories/CategoryList/index.js
+++ src/pages/student/categories/CategoryList/index.js
@@ -1,7 +1,111 @@
-import React from 'react';
+import React from "react";
+import Card from 'react-bootstrap/Card';
+import style from "./index.module.css"
 
-const CategoryList = () => {
-  return <div>Categories</div>;
-};
+function CategoryList() {
+    return (
+        <div>
+         <p className={style.title} >
+               Categories
+           </p>
+           
+        <div className={style.cardList}>
+          
+            <Card className={style.card}>
+            <Card.Header id={style.cardHeader}></Card.Header >
+            <Card.Body>
+                <div className={style.cardContent}>
+                   <center> <Card.Title>Web Development</Card.Title> </center>
+                </div>
+            </Card.Body>
+            </Card>
+
+
+            <Card className={style.card}>
+            <Card.Header id={style.cardHeader}></Card.Header >
+            <Card.Body>
+                <div className={style.cardContent}>
+                   <center> <Card.Title>Math</Card.Title> </center>
+                </div>
+            </Card.Body>
+            </Card>
+
+         
+            <Card className={style.card}>
+            <Card.Header id={style.cardHeader}></Card.Header >
+            <Card.Body>
+                <div className={style.cardContent}>
+                   <center> <Card.Title></Card.Title> </center>
+                </div>
+            </Card.Body>
+            </Card>
+
+         
+            <Card className={style.card}>
+            <Card.Header id={style.cardHeader}></Card.Header >
+            <Card.Body>
+                <div className={style.cardContent}>
+                   <center> <Card.Title></Card.Title> </center>
+                </div>
+            </Card.Body>
+            </Card>
+
+         
+            <Card className={style.card}>
+            <Card.Header id={style.cardHeader}></Card.Header >
+            <Card.Body>
+                <div className={style.cardContent}>
+                   <center> <Card.Title></Card.Title> </center>
+                </div>
+            </Card.Body>
+            </Card>
+
+         
+            <Card className={style.card}>
+            <Card.Header id={style.cardHeader}></Card.Header >
+            <Card.Body>
+                <div className={style.cardContent}>
+                   <center> <Card.Title></Card.Title> </center>
+                </div>
+            </Card.Body>
+            </Card>
+
+
+            
+            <Card className={style.card}>
+            <Card.Header id={style.cardHeader}></Card.Header >
+            <Card.Body>
+                <div className={style.cardContent}>
+                   <center> <Card.Title></Card.Title> </center>
+                </div>
+            </Card.Body>
+            </Card>
+
+                  
+
+
+            <Card className={style.card}>
+            <Card.Header id={style.cardHeader}></Card.Header >
+            <Card.Body>
+                <div className={style.cardContent}>
+                   <center> <Card.Title></Card.Title> </center>
+                </div>
+            </Card.Body>
+            </Card>
+
+                  
+
+            <Card className={style.card}>
+            <Card.Header id={style.cardHeader}></Card.Header >
+            <Card.Body>
+                <div className={style.cardContent}>
+                   <center> <Card.Title></Card.Title> </center>
+                </div>
+            </Card.Body>
+            </Card>
+        </div>
+        </div>
+    );
+}
 
 export default CategoryList;
--- src/pages/student/profile/ProfileDetail/index.js
+++ src/pages/student/profile/ProfileDetail/index.js
@@ -1,7 +1,29 @@
 import React from 'react';
+import Button from 'react-bootstrap/Button'
+import Form from 'react-bootstrap/Form'
+import { BiPencil } from 'react-icons/bi'
 
 const ProfileDetail = () => {
-  return <div>Profile</div>;
+  return (
+
+    <div>
+            <div className="detail-container">
+                <div className="detail-input">
+                    <Form.Label>Name</Form.Label>
+                    <Form.Control type="text" name="name" value="Jane Doe"/>
+                    {/* <BiPencil className="edit-button-one"/> */}
+                </div>
+                <div className="detail-input">
+                    <Form.Label>Email</Form.Label>
+                    <Form.Control type="email" name="email" value="janedoe@gmail.com"/>
+                    {/* <BiPencil className="edit-button-two"/> */}
+                </div>
+                <div className="detail-input changepass-button">
+                    <Button>Change Password</Button>
+                </div>
+            </div>
+        </div>
+  )
 };
 
 export default ProfileDetail;
