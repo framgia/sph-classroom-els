42a926e3298d699081af9985d8d079ee9fcc4b9c
John Harvey <89382692+JHarbee@users.noreply.github.com>
2021-11-04 10:24:56 +0800

    Added modal and revised class names

--- src/pages/student/main/PasswordReset/index.js
+++ src/pages/student/main/PasswordReset/index.js
@@ -1,43 +1,60 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Button from 'react-bootstrap/Button';
 import Container from 'react-bootstrap/Container';
 import Stack from 'react-bootstrap/Stack';
 import Form from 'react-bootstrap/Form';
 import style from './index.module.css';
-import Modal from './Modal';
+import Modal from 'react-bootstrap/Modal';
 
-const PasswordReset = () => (<center>
+const PasswordReset = () => {
 
-    <Container>
-        <Stack gap={2} className="col-md-5 mx-auto">
+    const [show, setShow] = useState(false);
 
-            <Form className={style.contentstyle}>
 
-                <div className={style.center} align="start">
-                    <Form.Group className="mb - 3" controlId="Email">
-                        <Form.Label>Reset Password</Form.Label>
-                        <Form.Control type="text" placeholder="Enter you Email" />
-                        <Form.Text className="text-muted">
-                        </Form.Text>
-                    </Form.Group>
+    return (<center>
 
-                    <center>
-                        <Button variant="primary" type="submit" id={style.button}>
-                            Send the recovery link
-                            <Modal />
-                        </Button>
+        <Container>
+            <Stack gap={2} className="col-md-5 mx-auto">
 
-                    </center>
+                <Form className={style.contentstyle}>
 
-                    <center>
-                        <div>
-                            <a href="login" className={style.cancel}>Cancel</a>
-                        </div>
-                    </center>
+                    <div className={style.center} align="start">
+                        <Form.Group className="mb - 3" controlId="Email">
+                            <Form.Label>Reset Password</Form.Label>
+                            <Form.Control type="text" placeholder="Enter you Email" />
+                            <Form.Text className="text-muted">
+                            </Form.Text>
+                        </Form.Group>
+
+                        <center>
+                            <Button onClick={() => { setShow(true); }} variant="primary" id={style.button}>
+                                <span>Send the recovery link</span>
+                            </Button>
+                        </center>
+
+                        <center>
+                            <div>
+                                <a href="login" className={style.cancel}>Cancel</a>
+                            </div>
+                        </center>
+                    </div>
+                </Form>
+            </Stack>
+        </Container>
+
+        <Modal show={show} onHide={() => { setShow(false); }} className={style.modalcenter}>
+            <Modal.Header closeButton>
+                <Modal.Title>
+                    Password Reset
+                </Modal.Title>
+            </Modal.Header>
+            <Modal.Body>
+                <div className={style.textmodal}>
+                    An email has been sent. Please click the link when you get it.
                 </div>
-            </Form>
-        </Stack>
-    </Container>
-</center>);
+            </Modal.Body>
+        </Modal>
+    </center>);
 
+};
 export default PasswordReset;
