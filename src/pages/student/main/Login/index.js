969731f3e34b2d0d4d63bb3f4c47603f758fbccd
Erron Intila <erron.intila@sun-asterisk.com>
2021-11-09 11:51:49 +0800

    Fix layout.

--- src/pages/student/main/Login/index.js
+++ src/pages/student/main/Login/index.js
@@ -38,10 +38,10 @@ const Login = () => {
   };
 
   return (
-    <div>
+    <div className="d-flex justify-content-center align-items-center">
       {showAlert && (
         <Alert
-          className="mx-4 my-4"
+          className={`${style.alert}`}
           variant="danger"
           onClose={() => setShowAlert(false)}
           dismissible
@@ -49,93 +49,95 @@ const Login = () => {
           {alertMessage}
         </Alert>
       )}
-      <Container style={{ marginTop: '200px' }}>
-        <Stack gap={2} className="col-md-5 mx-auto" id={style.log01}>
-          <Form onSubmit={handleSubmit(handleOnSubmit)}>
-            <div>
-              <Form.Group className="mb-3" controlId="email">
-                <Form.Label>
-                  <h5>Email Address</h5>
-                </Form.Label>
-                <Controller
-                  control={control}
-                  name="email"
-                  defaultValue=""
-                  render={({ field: { onChange, value, ref } }) => (
-                    <Form.Control
-                      onChange={onChange}
-                      value={value}
-                      ref={ref}
-                      className="cntrs"
-                      type="email"
-                      placeholder="Input Email Address"
-                      isInvalid={errors?.email}
-                      required
-                    />
-                  )}
-                />
-                <Form.Control.Feedback type="invalid">
-                  {errors?.email}
-                </Form.Control.Feedback>
-              </Form.Group>
+      <div>
+        <Container style={{ marginTop: '200px' }}>
+          <Stack gap={2} id={style.log01}>
+            <Form onSubmit={handleSubmit(handleOnSubmit)}>
+              <div>
+                <Form.Group className="mb-3" controlId="email">
+                  <Form.Label>
+                    <h5>Email Address</h5>
+                  </Form.Label>
+                  <Controller
+                    control={control}
+                    name="email"
+                    defaultValue=""
+                    render={({ field: { onChange, value, ref } }) => (
+                      <Form.Control
+                        onChange={onChange}
+                        value={value}
+                        ref={ref}
+                        className="cntrs"
+                        type="email"
+                        placeholder="Input Email Address"
+                        isInvalid={errors?.email}
+                        required
+                      />
+                    )}
+                  />
+                  <Form.Control.Feedback type="invalid">
+                    {errors?.email}
+                  </Form.Control.Feedback>
+                </Form.Group>
 
-              <Form.Group className="mb-3" controlId="password">
-                <Form.Label>
-                  <h5>Password</h5>
-                </Form.Label>
-                <Controller
-                  control={control}
-                  name="password"
-                  defaultValue=""
-                  render={({ field: { onChange, value, ref } }) => (
-                    <Form.Control
-                      onChange={onChange}
-                      value={value}
-                      ref={ref}
-                      className="cntrs"
-                      type="password"
-                      name="password"
-                      placeholder="************"
-                      isInvalid={errors?.password}
-                      required
-                    />
-                  )}
-                />
-                <Form.Control.Feedback type="invalid">
-                  {errors?.password}
-                </Form.Control.Feedback>
-              </Form.Group>
+                <Form.Group className="mb-3" controlId="password">
+                  <Form.Label>
+                    <h5>Password</h5>
+                  </Form.Label>
+                  <Controller
+                    control={control}
+                    name="password"
+                    defaultValue=""
+                    render={({ field: { onChange, value, ref } }) => (
+                      <Form.Control
+                        onChange={onChange}
+                        value={value}
+                        ref={ref}
+                        className="cntrs"
+                        type="password"
+                        name="password"
+                        placeholder="************"
+                        isInvalid={errors?.password}
+                        required
+                      />
+                    )}
+                  />
+                  <Form.Control.Feedback type="invalid">
+                    {errors?.password}
+                  </Form.Control.Feedback>
+                </Form.Group>
 
-              <p className={style.sign}>
-                <LinkContainer to="/reset-password">
-                  <a style={{ textDecoration: 'none' }} href="/#">
-                    Forgot password?
-                  </a>
-                </LinkContainer>
-              </p>
+                <p className={style.sign}>
+                  <LinkContainer to="/reset-password">
+                    <a style={{ textDecoration: 'none' }} href="/#">
+                      Forgot password?
+                    </a>
+                  </LinkContainer>
+                </p>
 
-              <center>
-                <Button className={style.Btncolor} type="submit">
-                  <h5>Sign In</h5>
-                </Button>
-              </center>
+                <center>
+                  <Button className={style.Btncolor} type="submit">
+                    <h5>Sign In</h5>
+                  </Button>
+                </center>
 
-              <center>
-                <div className="cnb">
-                  <p className={style.sign}>No Account Yet?</p>
-                  <h5 className={style.sign}>
-                    <LinkContainer to="/registration">
-                      <a style={{ textDecoration: 'none' }} href="/#">
-                        Sign Up
-                      </a>
-                    </LinkContainer>
-                  </h5>
-                </div>
-              </center>
-            </div>
-          </Form>
-        </Stack>
-      </Container>
+                <center>
+                  <div className="cnb">
+                    <p className={style.sign}>No Account Yet?</p>
+                    <h5 className={style.sign}>
+                      <LinkContainer to="/registration">
+                        <a style={{ textDecoration: 'none' }} href="/#">
+                          Sign Up
+                        </a>
+                      </LinkContainer>
+                    </h5>
+                  </div>
+                </center>
+              </div>
+            </Form>
+          </Stack>
+        </Container>
+      </div>
     </div>
   );
 };
