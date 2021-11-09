ea9f44f81a09e9edabe598ba4a7db4ca08dc527a
John Harvey <89382692+JHarbee@users.noreply.github.com>
2021-11-09 15:15:21 +0800

    added loading for the delay

--- src/pages/student/quizzes/QuizList/index.js
+++ src/pages/student/quizzes/QuizList/index.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
-import { Container, Row, Col, Button } from 'react-bootstrap';
+import { Container, Row, Col, Button, Spinner } from 'react-bootstrap';
 import Pagination from '../../../../components/Pagination';
 
 import QuestionGrid from './components/QuestionGrid';
@@ -37,6 +37,11 @@ const QuizList = () => {
     <Container className={style.container}>
       <Button className={style.backButton}>Back</Button>
       <p className={style.title}>{category?.title}</p>
+      {quizzes === null ? <div className={style.loading}>
+        <Spinner animation="border" role="status"></Spinner>
+        <span className={style.loadingWord}>Loading</span>
+      </div>
+        : ''}
       <Row xs={1} sm={2} md={3} lg={3}>
         {quizzes && quizzes.map((quiz, index) => {
           if (index + 1 > perPage) {
