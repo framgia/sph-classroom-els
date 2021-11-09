3d3222c1f68a32a3c9fef666bd0dff3758c716ee
ErickDoroy <89514595+ErickDoroy@users.noreply.github.com>
2021-11-09 15:02:13 +0800

    E_CLASSROOM-104 [Frontend] [Student] [Integration] User Authentication 2/2

--- src/pages/student/main/Registration/index.js
+++ src/pages/student/main/Registration/index.js
@@ -1,84 +1,199 @@
-import React from 'react';
-import Button from 'react-bootstrap/Button';
+import React, { useState } from 'react';
+import { LinkContainer } from 'react-router-bootstrap';
+import { useForm, Controller } from 'react-hook-form';
+import { Button, Alert } from 'react-bootstrap';
 import Container from 'react-bootstrap/Container';
 import Stack from 'react-bootstrap/Stack';
 import Form from 'react-bootstrap/Form';
+import Navbar from 'react-bootstrap/Navbar';
+
+
+import AuthApi from '../../../../api/Auth';
 import style from './index.module.css';
 
 const Registration = () => {
+
+  const { register, control, handleSubmit, data } = useForm();
+  const [errors, setErrors] = useState({});
+  const [showAlert, setShowAlert] = useState(false);
+  const [alertMessage, setAlertMessage] = useState('');
+
+  const showAlertDialog = (isShow, message) => {
+    setShowAlert(isShow);
+    setAlertMessage(message);
+  };
+
+  const handleOnSubmit = async ({ name, email, password, password_confirmation }) => {
+    try {
+      
+      const response = await AuthApi.register({ name, email, password, password_confirmation });
+      window.location = '/login';
+    } catch (error) {
+      console.log(error.response);
+      if (error?.response?.data?.errors) {
+        setErrors(error?.response?.data?.errors);
+      } else if (error?.response?.data?.error) {
+        showAlertDialog(true, error?.response?.data?.error?.message);
+      } else {
+        showAlertDialog(true, 'An error has occurred.');
+      }
+    }
+  };
+
   return (
-    <Container style={{ marginTop: '132px' }}>
-      <Stack gap={2} className="col-md-5 mx-auto" id={style.log01}>
-        <Form>
-          <div>
-            <Form.Group className="mb-3" controlId="CurrentPassword">
-              <Form.Label>
-                <h5>Name</h5>
-              </Form.Label>
-              <Form.Control
-                className="cntrs"
-                type="password"
-                placeholder="Input Username"
-              />
-              <Form.Text className="text-muted"></Form.Text>
-            </Form.Group>
+    <div>
+      <div>
+        <div>
+          <Navbar id={style.navbar} expand="lg">
+            <Container fluid>
+              <Navbar.Brand id={style.title}>E-Learning</Navbar.Brand>
+            </Container>
+          </Navbar>
+        </div>
+      </div>
+      {showAlert && (
+        <Alert
+          className="mx-4 my-4"
+          variant="danger"
+          onClose={() => setShowAlert(false)}
+          dismissible
+        >
+          {alertMessage}
+        </Alert>
+      )}
+      <Container style={{marginTop:'132px'}}>
+          <Stack gap={2} className="col-md-5 mx-auto" id={style.log01}>
+              <Form onSubmit={handleSubmit(handleOnSubmit)}>
+                  <div>
+                      <Form.Group className="mb-3" controlId="Name">
+                          <Form.Label>
+                            <h5>Name</h5>
+                          </Form.Label>
+                          <Controller
+                            control={control}
+                            name="name"
+                            defaultValue=""
+                            render={({ field: { onChange, value, ref } }) => (
+                              <Form.Control
+                                onChange={onChange}
+                                value={value}
+                                ref={ref}
+                                className="cntrs"
+                                type="text"
+                                placeholder="Input Name"
+                                isInvalid={errors?.name}
+                                required
+                              />
+                            )}
+                          />
+                          <Form.Control.Feedback type="invalid">
+                            {errors?.name}
+                          </Form.Control.Feedback>
+                      </Form.Group>
 
-            <Form.Group className="mb-3" controlId="CurrentPassword">
-              <Form.Label>
-                <h5>Email</h5>
-              </Form.Label>
-              <Form.Control
-                className="cntrs"
-                type="password"
-                placeholder="Input Username"
-              />
-              <Form.Text className="text-muted"></Form.Text>
-            </Form.Group>
+                      <Form.Group className="mb-3" controlId="Email">
+                          <Form.Label>
+                            <h5>Email</h5>
+                          </Form.Label>
+                          <Controller
+                            control={control}
+                            name="email"
+                            defaultValue=""
+                            render={({ field: { onChange, value, ref } }) => (
+                              <Form.Control
+                                onChange={onChange}
+                                value={value}
+                                ref={ref}
+                                className="cntrs"
+                                type="email"
+                                placeholder="Input Email Address"
+                                isInvalid={errors?.email}
+                                required
+                              />
+                            )}
+                          />
+                          <Form.Control.Feedback type="invalid">
+                            {errors?.email}
+                          </Form.Control.Feedback>
+                      </Form.Group>
 
-            <Form.Group className="mb-3" controlId="NewPassword">
-              <Form.Label>
-                <h5>Password</h5>
-              </Form.Label>
-              <Form.Control
-                className="cntrs"
-                type="password"
-                placeholder="************"
-              />
-              <Form.Text className="text-muted"></Form.Text>
-            </Form.Group>
+                      <Form.Group className="mb-3" controlId="Password">
+                          <Form.Label>
+                            <h5>Password</h5>
+                          </Form.Label>
+                          <Controller
+                            control={control}
+                            name="password"
+                            defaultValue=""
+                            render={({ field: { onChange, value, ref } }) => (
+                              <Form.Control
+                                onChange={onChange}
+                                value={value}
+                                ref={ref}
+                                className="cntrs"
+                                type="password"
+                                name="password"
+                                placeholder="************"
+                                isInvalid={errors?.password}
+                                required
+                              />
+                            )}
+                          />
+                          <Form.Control.Feedback type="invalid">
+                            {errors?.password}
+                          </Form.Control.Feedback>
+                      </Form.Group>
 
-            <Form.Group className="mb-3" controlId="NewPassword">
-              <Form.Label>
-                <h5>Confirm Password</h5>
-              </Form.Label>
-              <Form.Control
-                className="cntrs"
-                type="password"
-                placeholder="************"
-              />
-              <Form.Text className="text-muted"></Form.Text>
-            </Form.Group>
-
-            <center style={{ marginTop: '65px' }}>
-              <Button className={style.Btncolor} type="submit">
-                <h5>Sign Up</h5>
-              </Button>
-            </center>
-
-            <center>
-              <div className="cnb">
-                <p className={style.sign}>Already have an Account?</p>
-                <h5 className={style.sign}>
-                  <a style={{ textDecoration: 'none' }} href="/#">
-                    Sign In
-                  </a>
-                </h5>
-              </div>
-            </center>
-          </div>
-        </Form>
-      </Stack>
-    </Container>
+                      <Form.Group className="mb-3" controlId="PasswordConfirmation">
+                          <Form.Label>
+                            <h5>Confirm Password</h5>
+                          </Form.Label>
+                          <Controller
+                            control={control}
+                            name="password_confirmation"
+                            defaultValue=""
+                            render={({ field: { onChange, value, ref } }) => (
+                              <Form.Control
+                                onChange={onChange}
+                                value={value}
+                                ref={ref}
+                                className="cntrs"
+                                type="password"
+                                name="password_confirmation"
+                                placeholder="************"
+                                isInvalid={errors?.password_confirmation}
+                                required
+                              />
+                            )}
+                          />
+                          <Form.Control.Feedback type="invalid">
+                            {errors?.password_confirmation}
+                          </Form.Control.Feedback>
+                      </Form.Group>
+                      
+                      <center style={{marginTop:'65px'}}>
+                          <Button className={style.Btncolor} type="submit">
+                          <h5>Sign Up</h5>
+                          </Button>
+                      </center>
+                      
+                      <center>
+                          <div className="cnb">
+                            <p className={style.sign}>Already have an Account?</p>
+                            <h5 className={style.sign}>
+                              <LinkContainer to="/registration">
+                                <a style={{ textDecoration: 'none' }} href="/#">
+                                  Sign Up
+                                </a>
+                              </LinkContainer>
+                            </h5>
+                          </div>
+                      </center>
+                  </div>
+              </Form>
+          </Stack>
+      </Container >
+    </div>
   );
 };
 
