7bcdcea1fa625172863e7208c28c804ff6c29e62
Erron Intila <erron.intila@sun-asterisk.com>
2021-11-09 08:14:37 +0800

    Add validation.

--- src/pages/student/main/Login/index.js
+++ src/pages/student/main/Login/index.js
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import { LinkContainer } from 'react-router-bootstrap';
 import { useForm, Controller } from 'react-hook-form';
-import Button from 'react-bootstrap/Button';
+import { Button, Alert } from 'react-bootstrap';
 import Container from 'react-bootstrap/Container';
 import Stack from 'react-bootstrap/Stack';
 import Form from 'react-bootstrap/Form';
@@ -10,10 +10,14 @@ import Cookies from 'js-cookie';
 import AuthApi from '../../../../api/Auth';
 
 const Login = () => {
-  const { register, control, handleSubmit, errors, data } = useForm();
+  const { register, control, handleSubmit, data } = useForm();
+  const [errors, setErrors] = useState({});
+  const [showAlert, setShowAlert] = useState(false);
+  const [alertMessage, setAlertMessage] = useState('');
 
-  const errorse = {
-    // email: 'Sample Error',
+  const showAlertDialog = (isShow, message) => {
+    setShowAlert(isShow);
+    setAlertMessage(message);
   };
 
   const handleOnSubmit = async ({ email, password }) => {
@@ -23,91 +27,116 @@ const Login = () => {
       window.location = '/';
     } catch (error) {
       console.log(error.response);
+      if (error?.response?.data?.errors) {
+        setErrors(error?.response?.data?.errors);
+      } else if (error?.response?.data?.error) {
+        showAlertDialog(true, error?.response?.data?.error?.message);
+      } else {
+        showAlertDialog(true, 'An error has occurred.');
+      }
     }
   };
 
   return (
-    <Container style={{ marginTop: '200px' }}>
-      <Stack gap={2} className="col-md-5 mx-auto" id={style.log01}>
-        <Form onSubmit={handleSubmit(handleOnSubmit)}>
-          <div>
-            <Form.Group className="mb-3" controlId="email">
-              <Form.Label>
-                <h5>Email Address</h5>
-              </Form.Label>
-              <Controller
-                control={control}
-                name="email"
-                defaultValue=""
-                render={({ field: { onChange, value, ref } }) => (
-                  <Form.Control
-                    onChange={onChange}
-                    value={value}
-                    ref={ref}
-                    className="cntrs"
-                    type="email"
-                    placeholder="Input Email Address"
-                    isInvalid={errorse?.email}
-                  />
-                )}
-              />
-              <Form.Control.Feedback type="invalid">
-                {errorse?.email}
-              </Form.Control.Feedback>
-            </Form.Group>
+    <div>
+      {showAlert && (
+        <Alert
+          className="mx-4 my-4"
+          variant="danger"
+          onClose={() => setShowAlert(false)}
+          dismissible
+        >
+          {alertMessage}
+        </Alert>
+      )}
+      <Container style={{ marginTop: '200px' }}>
+        <Stack gap={2} className="col-md-5 mx-auto" id={style.log01}>
+          <Form onSubmit={handleSubmit(handleOnSubmit)}>
+            <div>
+              <Form.Group className="mb-3" controlId="email">
+                <Form.Label>
+                  <h5>Email Address</h5>
+                </Form.Label>
+                <Controller
+                  control={control}
+                  name="email"
+                  defaultValue=""
+                  render={({ field: { onChange, value, ref } }) => (
+                    <Form.Control
+                      onChange={onChange}
+                      value={value}
+                      ref={ref}
+                      className="cntrs"
+                      type="email"
+                      placeholder="Input Email Address"
+                      isInvalid={errors?.email}
+                      required
+                    />
+                  )}
+                />
+                <Form.Control.Feedback type="invalid">
+                  {errors?.email}
+                </Form.Control.Feedback>
+              </Form.Group>
 
-            <Form.Group className="mb-3" controlId="password">
-              <Form.Label>
-                <h5>Password</h5>
-              </Form.Label>
-              <Controller
-                control={control}
-                name="password"
-                defaultValue=""
-                render={({ field: { onChange, value, ref } }) => (
-                  <Form.Control
-                    onChange={onChange}
-                    value={value}
-                    ref={ref}
-                    className="cntrs"
-                    type="password"
-                    name="password"
-                    placeholder="************"
-                  />
-                )}
-              />
-            </Form.Group>
+              <Form.Group className="mb-3" controlId="password">
+                <Form.Label>
+                  <h5>Password</h5>
+                </Form.Label>
+                <Controller
+                  control={control}
+                  name="password"
+                  defaultValue=""
+                  render={({ field: { onChange, value, ref } }) => (
+                    <Form.Control
+                      onChange={onChange}
+                      value={value}
+                      ref={ref}
+                      className="cntrs"
+                      type="password"
+                      name="password"
+                      placeholder="************"
+                      isInvalid={errors?.password}
+                      required
+                    />
+                  )}
+                />
+                <Form.Control.Feedback type="invalid">
+                  {errors?.password}
+                </Form.Control.Feedback>
+              </Form.Group>
 
-            <p className={style.sign}>
-              <LinkContainer to="/reset-password">
-                <a style={{ textDecoration: 'none' }} href="/#">
-                  Forgot password?
-                </a>
-              </LinkContainer>
-            </p>
+              <p className={style.sign}>
+                <LinkContainer to="/reset-password">
+                  <a style={{ textDecoration: 'none' }} href="/#">
+                    Forgot password?
+                  </a>
+                </LinkContainer>
+              </p>
 
-            <center>
-              <Button className={style.Btncolor} type="submit">
-                <h5>Sign In</h5>
-              </Button>
-            </center>
+              <center>
+                <Button className={style.Btncolor} type="submit">
+                  <h5>Sign In</h5>
+                </Button>
+              </center>
 
-            <center>
-              <div className="cnb">
-                <p className={style.sign}>No Account Yet?</p>
-                <h5 className={style.sign}>
-                  <LinkContainer to="/registration">
-                    <a style={{ textDecoration: 'none' }} href="/#">
-                      Sign Up
-                    </a>
-                  </LinkContainer>
-                </h5>
-              </div>
-            </center>
-          </div>
-        </Form>
-      </Stack>
-    </Container>
+              <center>
+                <div className="cnb">
+                  <p className={style.sign}>No Account Yet?</p>
+                  <h5 className={style.sign}>
+                    <LinkContainer to="/registration">
+                      <a style={{ textDecoration: 'none' }} href="/#">
+                        Sign Up
+                      </a>
+                    </LinkContainer>
+                  </h5>
+                </div>
+              </center>
+            </div>
+          </Form>
+        </Stack>
+      </Container>
+    </div>
   );
 };
 
