21478e3ab41bca75a5f8bb84e0a28cbc5074ceaa
unknown <johnpaul.lim@sun-asterisk.com>
2021-11-09 16:17:14 +0800

    E_CLASSROOM-113 [Integration] [Student] Categories

--- src/pages/student/categories/CategoryList/index.js
+++ src/pages/student/categories/CategoryList/index.js
@@ -1,111 +1,51 @@
-import React from "react";
+import React, { useState, useEffect } from 'react';
 import Card from 'react-bootstrap/Card';
+import Spinner from 'react-bootstrap/Spinner'
 import style from "./index.module.css"
 
+import CategoryApi from '../../../../api/Category';
+
 function CategoryList() {
+    const [categories, setCategories] = useState(null)
+
+    useEffect(() => {
+        CategoryApi.getAll()
+        .then(({ data }) => {
+            setCategories(data.data)
+        })
+    }, [])
+
+    const renderCatList = () => {
+        return categories.map((category, idx) => {
+            return (
+                <Card className={style.card} key={ idx }>
+                <Card.Header id={style.cardHeader}></Card.Header >
+                <Card.Body>
+                    <div className={style.cardContent}>
+                    <center> <Card.Title>{ category.name }</Card.Title> </center>
+                    </div>
+                </Card.Body>
+                </Card>
+            )
+        })
+    }
+
     return (
         <div>
-         <p className={style.title} >
-               Categories
-           </p>
-           
-        <div className={style.cardList}>
-          
-            <Card className={style.card}>
-            <Card.Header id={style.cardHeader}></Card.Header >
-            <Card.Body>
-                <div className={style.cardContent}>
-                   <center> <Card.Title>Web Development</Card.Title> </center>
+            <p className={style.title}>Categories</p>
+            {categories === null ? 
+                <div className={style.loading}>
+                    <Spinner animation="border" role="status"></Spinner>
+                    <span className={style.loadingWord}>Loading</span>
                 </div>
-            </Card.Body>
-            </Card>
-
-
-            <Card className={style.card}>
-            <Card.Header id={style.cardHeader}></Card.Header >
-            <Card.Body>
-                <div className={style.cardContent}>
-                   <center> <Card.Title>Math</Card.Title> </center>
+                :
+                <div className={style.cardList}>
+                    {renderCatList()}
                 </div>
-            </Card.Body>
-            </Card>
-
-         
-            <Card className={style.card}>
-            <Card.Header id={style.cardHeader}></Card.Header >
-            <Card.Body>
-                <div className={style.cardContent}>
-                   <center> <Card.Title></Card.Title> </center>
-                </div>
-            </Card.Body>
-            </Card>
-
-         
-            <Card className={style.card}>
-            <Card.Header id={style.cardHeader}></Card.Header >
-            <Card.Body>
-                <div className={style.cardContent}>
-                   <center> <Card.Title></Card.Title> </center>
-                </div>
-            </Card.Body>
-            </Card>
-
-         
-            <Card className={style.card}>
-            <Card.Header id={style.cardHeader}></Card.Header >
-            <Card.Body>
-                <div className={style.cardContent}>
-                   <center> <Card.Title></Card.Title> </center>
-                </div>
-            </Card.Body>
-            </Card>
-
-         
-            <Card className={style.card}>
-            <Card.Header id={style.cardHeader}></Card.Header >
-            <Card.Body>
-                <div className={style.cardContent}>
-                   <center> <Card.Title></Card.Title> </center>
-                </div>
-            </Card.Body>
-            </Card>
-
-
-            
-            <Card className={style.card}>
-            <Card.Header id={style.cardHeader}></Card.Header >
-            <Card.Body>
-                <div className={style.cardContent}>
-                   <center> <Card.Title></Card.Title> </center>
-                </div>
-            </Card.Body>
-            </Card>
-
-                  
-
-
-            <Card className={style.card}>
-            <Card.Header id={style.cardHeader}></Card.Header >
-            <Card.Body>
-                <div className={style.cardContent}>
-                   <center> <Card.Title></Card.Title> </center>
-                </div>
-            </Card.Body>
-            </Card>
-
-                  
-
-            <Card className={style.card}>
-            <Card.Header id={style.cardHeader}></Card.Header >
-            <Card.Body>
-                <div className={style.cardContent}>
-                   <center> <Card.Title></Card.Title> </center>
-                </div>
-            </Card.Body>
-            </Card>
-        </div>
+            }
         </div>
     );
 }
 
 export default CategoryList;
+
