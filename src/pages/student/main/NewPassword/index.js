00064520e6fd6ad93d24749457cfed8df6bbcf11
Joash Cañete <joash.canete@sun-asterisk.com>
2021-11-10 14:30:15 +0800

    E_CLASSROOM-118 [Integration] [Student] Password Reset

--- src/pages/student/main/NewPassword/index.js
+++ src/pages/student/main/NewPassword/index.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 import Button from "react-bootstrap/Button";
 import Container from "react-bootstrap/Container";
 import Stack from "react-bootstrap/Stack";
@@ -6,26 +6,29 @@ import Form from "react-bootstrap/Form";
 import style from "./index.module.css";
 import PasswordResetApi from "../../../../api/PasswordReset";
 import { useForm, Controller } from "react-hook-form";
-import Cookies from 'js-cookie';
+import Cookies from "js-cookie";
+import { Alert } from "react-bootstrap";
 
 const NewPassword = () => {
-    const { control, handleSubmit, data } = useForm();
-    const [errors, setErrors] = useState({});
-    const [showAlert, setShowAlert] = useState(false);
-    const [alertMessage, setAlertMessage] = useState('');
+  const { control, handleSubmit, data } = useForm();
+  const [errors, setErrors] = useState({});
+  const [showAlert, setShowAlert] = useState(false);
+  const [alertMessage, setAlertMessage] = useState("");
+
+  const queryParams = new URLSearchParams(window.location.search); 
+  const token = queryParams.get('token');
 
   const showAlertDialog = (isShow, message) => {
     setShowAlert(isShow);
     setAlertMessage(message);
   };
 
-  const handleOnSubmit = async ({ password, password_confirmation, token }) => {
-    try {
-        // console.log( email, password, password_confirmation, token );
-
-      const response = await PasswordResetApi.newPassword({ password, password_confirmation, token });
-      Cookies.set('access_token', response.data.token);
-      window.location = '/';
+  const handleOnSubmit = async ({ email, password, password_confirmation }) => {
+    try {      
+      const values = { email: email, password: password, password_confirmation: password_confirmation, token: token };
+      const response = await PasswordResetApi.resetPassword( values );
+      Cookies.set("access_token", response.data.token);
+      window.location = "/";
     } catch (error) {
       console.log(error.response);
       if (error?.response?.data?.errors) {
@@ -33,27 +36,53 @@ const NewPassword = () => {
       } else if (error?.response?.data?.error) {
         showAlertDialog(true, error?.response?.data?.error?.message);
       } else {
-        showAlertDialog(true, 'An error has occurred.');
+        showAlertDialog(true, "An error has occurred.");
       }
     }
   };
-  
+
   return (
-  <center>
-    <Container>
-      <Stack gap={2} className="col-md-5 mx-auto">
-        <Form
-          onSubmit={handleSubmit(handleOnSubmit)}
-          className={style.contentstyle}
-        >
-          <div align="start" className={style.suggestion}>
-            Create a new password that is at least 6 characters long. A strong
-            password is a combination of letters, numbers, and symbols.
-          </div>
-          <div className={style.left} align="start">
+    <center>
+      <Container>
+        <Stack gap={2} className="col-md-5 mx-auto">
+          <Form
+            onSubmit={handleSubmit(handleOnSubmit)}
+            className={style.contentstyle}
+          >
+            <div align="start" className={style.suggestion}>
+              Create a new password that is at least 6 characters long. A strong
+              password is a combination of letters, numbers, and symbols.
+            </div>
+            <div className={style.left} align="start">
             <Form.Group className="mb-3" controlId="NewPassword">
-              <Form.Label>New Password</Form.Label>
-              <Controller
+                <Form.Label>Email</Form.Label>
+                  <Controller
+                    control={control}
+                    name="email"
+                    defaultValue=""
+                    render={({ field: { onChange, value, ref } }) => (
+                      <Form.Control
+                        onChange={onChange}
+                        value={value}
+                        ref={ref}
+                        className="cntrs"
+                        type="email"
+                        placeholder="Input Email Address"
+                        isInvalid={errors?.email}
+                        required
+                      />
+                    )}
+                  />
+                  <Form.Control.Feedback type="invalid">
+                    {errors?.email}
+                  </Form.Control.Feedback>
+                </Form.Group>
+                </div>
+
+            <div className={style.left} align="start">
+              <Form.Group className="mb-3" controlId="NewPassword">
+                <Form.Label>New Password</Form.Label>
+                <Controller
                   control={control}
                   name="password"
                   defaultValue=""
@@ -65,7 +94,7 @@ const NewPassword = () => {
                       className="cntrs"
                       type="password"
                       placeholder="***********"
-                      isInvalid={errors?.email}
+                      isInvalid={errors?.password}
                       required
                     />
                   )}
@@ -73,11 +102,11 @@ const NewPassword = () => {
                 <Form.Control.Feedback type="invalid">
                   {errors?.password}
                 </Form.Control.Feedback>
-            </Form.Group>
+              </Form.Group>
 
-            <Form.Group className="mb-3" controlId="ConfirmPassword">
-              <Form.Label>Confirm Password</Form.Label>
-              <Controller
+              <Form.Group className="mb-3" controlId="ConfirmPassword">
+                <Form.Label>Confirm Password</Form.Label>
+                <Controller
                   control={control}
                   name="password_confirmation"
                   defaultValue=""
@@ -88,29 +117,28 @@ const NewPassword = () => {
                       ref={ref}
                       className="cntrs"
                       type="password"
-                      placeholder="***********"
-                      isInvalid={errors?.email}
+                      name="password_confirmation"
+                      placeholder="************"
+                      isInvalid={errors?.password_confirmation}
                       required
                     />
                   )}
                 />
                 <Form.Control.Feedback type="invalid">
-                  {errors?.password}
+                  {errors?.password_confirmation}
                 </Form.Control.Feedback>
-            </Form.Group>
+              </Form.Group>
 
-            <center>
-              <Button type="submit" id={style.button}>
-                <span className={style.textbutton}>
-                  Continue
-                </span>
-              </Button>
-            </center>
-          </div>
-        </Form>
-      </Stack>
-    </Container>
-  </center>
+              <center>
+                <Button type="submit" id={style.button}>
+                  <a className={style.textbutton}>Continue</a>
+                </Button>
+              </center>
+            </div>
+          </Form>
+        </Stack>
+      </Container>
+    </center>
   );
 };
 
