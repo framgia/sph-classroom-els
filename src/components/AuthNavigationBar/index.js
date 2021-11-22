ee6a014cef5101f7bac55cff9514c8f8ad032b34
ErickDoroy <89514595+ErickDoroy@users.noreply.github.com>
2021-11-22 16:03:49 +0800

    Refactor the cod

--- src/components/AuthNavigationBar/index.js
+++ src/components/AuthNavigationBar/index.js
@@ -1,13 +1,22 @@
 import React from 'react';
 import Navbar from 'react-bootstrap/Navbar';
 import { Container } from 'react-bootstrap';
+import { LinkContainer } from 'react-router-bootstrap';
 import style from './index.module.css';
 
 const AuthNavigationBar = () => {
   return (
     <Navbar id={style.navbar} expand="lg">
       <Container fluid>
-        <Navbar.Brand id={style.title}>E-Learning</Navbar.Brand>
+        <Navbar.Brand id={style.title}>E-LEARNING</Navbar.Brand>
+        <div>
+          <LinkContainer to="/login">
+            <Navbar.Brand id={style.Subtitle}>Login</Navbar.Brand>
+          </LinkContainer>
+          <LinkContainer to="/registration">
+            <Navbar.Brand id={style.Subtitle}>Register</Navbar.Brand>
+          </LinkContainer >
+        </div>
       </Container>
     </Navbar>
   );
--- src/pages/student/main/Dashboard/index.js
+++ src/pages/student/main/Dashboard/index.js
@@ -2,38 +2,113 @@ import React from 'react';
 import { BiBookAlt } from 'react-icons/bi';
 import { HiOutlineUserAdd } from 'react-icons/hi';
 import { HiOutlineClipboardList } from 'react-icons/hi';
+import { CgTimer } from 'react-icons/cg';
 
 import style from './index.module.css';
 
+
 const Dashboard = () => {
   return (
-    <div>
+    <div style={{padding: '0px 196px'}}>
       <h2 className={style.h2_style}>Recent</h2>
       <div className={style.bg}>
         <div>
-          <div className={style.forContainerBar}><h6 className={style.titleText}>TOPIC:</h6> Web Development
+          <div className={style.forContainerBar}>
+            <h6 className={style.titleText}>HTML</h6>
+            <CgTimer size='10px' style={{margin: '0px 4px 3px 200px'}}/>5 Mins
           </div>
           <div className={style.forContent_boxs}>
-            <div className={style.box_small}>
-              <p className={style.forTexts}>Score: 10/10<br/>Time: 5 Mins</p>
+            <div>
+              <table style={{width: '100%'}}>
+                <tr>
+                  <td className={style.listTable}>Attempt</td>
+                  <td className={style.forSeccolum}>8</td>
+                </tr>
+                <tr>
+                  <td className={style.listTable}>Highest Score</td>
+                  <td className={style.forSeccolum}>8/10</td>
+                </tr>
+                <tr>
+                  <td id={style.listTable}>Latest Score</td>
+                  <td className={style.forSeccolum2}>7/10</td>
+                </tr>    
+              </table>
+              <p className={style.retake}>Retake Quiz</p>
             </div>
           </div>
         </div>
         <div>
-          <div className={style.forContainerBar}><h6 className={style.titleText}>TOPIC:</h6> Data Structure
+          <div className={style.forContainerBar}>
+            <h6 className={style.titleText}>Linked List</h6>
+            <CgTimer size='10px' style={{margin: '0px 4px 3px 169px'}}/>5 Mins
           </div>
           <div className={style.forContent_boxs}>
-            <   div className={style.box_small}>
-              <p className={style.forTexts}>Score: 6/10<br/>Time: 5 Mins</p>
+            <div>
+              <table style={{width: '100%'}}>
+                <tr>
+                  <td className={style.listTable}>Attempt</td>
+                  <td className={style.forSeccolum}>8</td>
+                </tr>
+                <tr>
+                  <td className={style.listTable}>Highest Score</td>
+                  <td className={style.forSeccolum}>8/10</td>
+                </tr>
+                <tr>
+                  <td id={style.listTable}>Latest Score</td>
+                  <td className={style.forSeccolum2}>7/10</td>
+                </tr>    
+              </table>
+              <p className={style.retake}>Retake Quiz</p>
             </div>
           </div>
         </div>
         <div>
-          <div className={style.forContainerBar}><h6 className={style.titleText}>Visited:</h6> Friends
+          <div className={style.forContainerBar}>
+            <h6 className={style.titleText}>Encapsulation</h6>
+            <CgTimer size='10px' style={{margin: '0px 4px 3px 140px'}}/>5 Mins
           </div>
           <div className={style.forContent_boxs}>
-            <div className={style.box_small}>
-              <p className={style.forTexts}>Jhon Paul Lim</p>
+            <div>
+              <table style={{width: '100%'}}>
+                <tr>
+                  <td className={style.listTable}>Attempt</td>
+                  <td className={style.forSeccolum}>8</td>
+                </tr>
+                <tr>
+                  <td className={style.listTable}>Highest Score</td>
+                  <td className={style.forSeccolum}>8/10</td>
+                </tr>
+                <tr>
+                  <td id={style.listTable}>Latest Score</td>
+                  <td className={style.forSeccolum2}>7/10</td>
+                </tr>    
+              </table>
+              <p className={style.retake}>Retake Quiz</p>
+            </div>
+          </div>
+        </div>
+        <div>
+          <div className={style.forContainerBar}>
+            <h6 className={style.titleText}>CSS</h6>
+            <CgTimer size='10px' style={{margin: '0px 4px 3px 219px'}}/>5 Mins
+          </div>
+          <div className={style.forContent_boxs}>
+            <div>
+              <table style={{width: '100%'}}>
+                <tr>
+                  <td className={style.listTable}>Attempt</td>
+                  <td className={style.forSeccolum}>8</td>
+                </tr>
+                <tr>
+                  <td className={style.listTable}>Highest Score</td>
+                  <td className={style.forSeccolum}>8/10</td>
+                </tr>
+                <tr>
+                  <td id={style.listTable}>Latest Score</td>
+                  <td className={style.forSeccolum2}>7/10</td>
+                </tr>    
+              </table>
+              <p className={style.retake}>Retake Quiz</p>
             </div>
           </div>
         </div>
@@ -44,35 +119,55 @@ const Dashboard = () => {
             <p className={style.titleText}>Categories Learned</p>
             <button className={style.btnVeiw}>View all</button>
           </div>
-          <div className={style.forContent_box}>
+          <div className={`${style.forContent_box} ${style.forScroll}`}>
             <table style={{width: '100%'}}>
               <tr>
-                <td><BiBookAlt size="30px"/>HTML</td>
-                <td className={style.forSeccolum}>Quizzes Taken: 5 out of 10</td>
+                <td className={style.listTable}><BiBookAlt size="20px" style={{margin: '0px 17px 5px 17px'}}/>HTML</td>
+                <td className={style.forSeccolum}>1 minute ago</td>
               </tr>
               <tr>
-                <td><BiBookAlt size="30px"/>JavaScript</td>
-                <td className={style.forSeccolum}>Quizzes Taken: 5 out of 10</td>
+                <td className={style.listTable}><BiBookAlt size="20px" style={{margin: '0px 17px 5px 17px'}}/>JavaScript</td>
+                <td className={style.forSeccolum}>1 minute ago</td>
               </tr>
               <tr>
-                <td><BiBookAlt size="30px"/>Algebra</td>
-                <td className={style.forSeccolum}>Quizzes Taken: 5 out of 10</td>
+                <td className={style.listTable}><BiBookAlt size="20px" style={{margin: '0px 17px 5px 17px'}}/>Algebra</td>
+                <td className={style.forSeccolum}>1 minute ago</td>
               </tr>
               <tr>
-                <td><BiBookAlt size="30px"/>Algebra</td>
-                <td className={style.forSeccolum}>Quizzes Taken: 5 out of 10</td>
+                <td className={style.listTable}><BiBookAlt size="20px" style={{margin: '0px 17px 5px 17px'}}/>Algebra</td>
+                <td className={style.forSeccolum}>1 minute ago</td>
               </tr>
               <tr>
-                <td><BiBookAlt size="30px"/>Science</td>
-                <td className={style.forSeccolum}>Quizzes Taken: 5 out of 10</td>
+                <td className={style.listTable}><BiBookAlt size="20px" style={{margin: '0px 17px 5px 17px'}}/>Science</td>
+                <td className={style.forSeccolum}>1 minute ago</td>
               </tr>
               <tr>
-                <td><BiBookAlt size="30px"/>Mathematics</td>
-                <td className={style.forSeccolum}>Quizzes Taken: 5 out of 10</td>
+                <td className={style.listTable}><BiBookAlt size="20px" style={{margin: '0px 17px 5px 17px'}}/>Mathematics</td>
+                <td className={style.forSeccolum}>1 minute ago</td>
               </tr>
               <tr>
-                <tb><BiBookAlt size="30px"/>English</tb>
-                <td className={style.forSeccolum}>Quizzes Taken: 5 out of 10</td>
+                <tb className={style.listTable}><BiBookAlt size="20px" style={{margin: '0px 17px 5px 17px'}}/>English</tb>
+                <td className={style.forSeccolum}>1 minute ago</td>
+              </tr>
+              <tr>
+                <tb className={style.listTable}><BiBookAlt size="20px" style={{margin: '0px 17px 5px 17px'}}/>English</tb>
+                <td className={style.forSeccolum}>1 minute ago</td>
+              </tr>
+              <tr>
+                <tb className={style.listTable}><BiBookAlt size="20px" style={{margin: '0px 17px 5px 17px'}}/>English</tb>
+                <td className={style.forSeccolum}>1 minute ago</td>
+              </tr>
+              <tr>
+                <tb className={style.listTable}><BiBookAlt size="20px" style={{margin: '0px 17px 5px 17px'}}/>English</tb>
+                <td className={style.forSeccolum}>1 minute ago</td>
+              </tr>
+              <tr>
+                <tb className={style.listTable}><BiBookAlt size="20px" style={{margin: '0px 17px 5px 17px'}}/>English</tb>
+                <td className={style.forSeccolum}>1 minute ago</td>
+              </tr>
+              <tr>
+                <tb className={style.listTable}><BiBookAlt size="20px" style={{margin: '0px 17px 5px 17px'}}/>English</tb>
+                <td className={style.forSeccolum}>1 minute ago</td>
               </tr>
             </table>
           </div>
@@ -81,13 +176,58 @@ const Dashboard = () => {
           <div className={style.forContainerBar2}><p className={style.titleText}>Friend&#39;s Activities</p>
           </div>
           <div className={`${style.forContent_box} ${style.forScroll}`}>
-            <p><HiOutlineUserAdd size="30px"/><h6 className={style.titleText}>Paul</h6>Followed Jhon Doe<br/>1 min ago</p>
-            <p><HiOutlineClipboardList size="30px"/><h6 className={style.titleText}>Erick</h6>Followed Jhon Doe<br/>1 min ago</p>
-            <p><HiOutlineUserAdd size="30px"/><h6 className={style.titleText}>Ramon</h6>Followed Jhon Doe<br/>1 min ago</p>
-            <p><HiOutlineClipboardList size="30px"/><h6 className={style.titleText}>Paul</h6>Followed Jhon Doe<br/>1 min ago</p>
-            <p><HiOutlineUserAdd size="30px"/><h6 className={style.titleText}>Machiel</h6>Followed Jhon Doe<br/>1 min ago</p>
-            <p><HiOutlineClipboardList size="30px"/><h6 className={style.titleText}>Paul</h6>Followed Jhon Doe<br/>1 min ago</p>
-            <p><HiOutlineUserAdd size="30px"/><h6 className={style.titleText}>Jhon</h6>Followed Jhon Doe<br/>2 min ago</p>
+            <table style={{width: '100%'}}>
+              <tr>
+                <td className={style.listTable}><HiOutlineUserAdd size="20px" style={{margin: '0px 17px 5px 17px'}}/>Paul Followed Jhon Doe</td>
+                <td className={style.forSeccolum}>1 minute ago</td>
+              </tr>
+              <tr>
+                <td className={style.listTable}><HiOutlineClipboardList size="20px" style={{margin: '0px 17px 5px 17px'}}/>Ramon Followed Jhon Doe</td>
+                <td className={style.forSeccolum}>1 minute ago</td>
+              </tr>
+              <tr>
+                <td className={style.listTable}><HiOutlineUserAdd size="20px" style={{margin: '0px 17px 5px 17px'}}/>Paul Followed Jhon Doe</td>
+                <td className={style.forSeccolum}>16 minute ago</td>
+              </tr>
+              <tr>
+                <td className={style.listTable}><HiOutlineUserAdd size="20px" style={{margin: '0px 17px 5px 17px'}}/>Einstein answered Physics Quiz</td>
+                <td className={style.forSeccolum}>18 minute ago</td>
+              </tr>
+              <tr>
+                <td className={style.listTable}><HiOutlineUserAdd size="20px" style={{margin: '0px 17px 5px 17px'}}/>Harvey followed Gen</td>
+                <td className={style.forSeccolum}>24 minute ago</td>
+              </tr>
+              <tr>
+                <td className={style.listTable}><HiOutlineUserAdd size="20px" style={{margin: '0px 17px 5px 17px'}}/>John answered Programming Quiz</td>
+                <td className={style.forSeccolum}>30 minute ago</td>
+              </tr>
+              <tr>
+                <tb className={style.listTable}><HiOutlineClipboardList size="20px" style={{margin: '0px 17px 5px 17px'}}/>Cinderella answered History Quiz</tb>
+                <td className={style.forSeccolum}>35 minute ago</td>
+              </tr>
+              <tr>
+                <tb className={style.listTable}><HiOutlineClipboardList size="20px" style={{margin: '0px 17px 5px 17px'}}/>Cruz answered History Quiz</tb>
+                <td className={style.forSeccolum}>35 minute ago</td>
+              </tr>
+              <tr>
+                <tb className={style.listTable}><HiOutlineClipboardList size="20px" style={{margin: '0px 17px 5px 17px'}}/>Dela Juan answered History Quiz</tb>
+                <td className={style.forSeccolum}>35 minute ago</td>
+              </tr>
+              <tr>
+                <tb className={style.listTable}><HiOutlineClipboardList size="20px" style={{margin: '0px 17px 5px 17px'}}/>Ramon answered History Quiz</tb>
+                <td className={style.forSeccolum}>35 minute ago</td>
+              </tr>
+              <tr>
+                <td className={style.listTable}><HiOutlineUserAdd size="20px" style={{margin: '0px 17px 5px 17px'}}/>Kian Followed Jhon Doe</td>
+                <td className={style.forSeccolum}>42 minute ago</td>
+              </tr><tr>
+                <td className={style.listTable}><HiOutlineUserAdd size="20px" style={{margin: '0px 17px 5px 17px'}}/>Juan Ramon Followed Jhon Doe</td>
+                <td className={style.forSeccolum}>44 minute ago</td>
+              </tr><tr>
+                <td className={style.listTable}><HiOutlineUserAdd size="20px" style={{margin: '0px 17px 5px 17px'}}/>Justine Followed Jhon Doe</td>
+                <td className={style.forSeccolum}>55 minute ago</td>
+              </tr>
+            </table>
           </div>
         </div>
       </div>
