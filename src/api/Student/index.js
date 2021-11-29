70e132cd9796156a6c3964427f1c94ab235c911f
ErickDoroy <89514595+ErickDoroy@users.noreply.github.com>
2021-11-29 16:42:49 +0800

    E_CLASSROOM-179 [FE] [Integration] [Student] Student List Page

---
+++ src/api/Student/index.js
@@ -0,0 +1,41 @@
+import API from '../Base';
+
+const StudentsApi = {
+  getAll: ({ ...params }) => {
+    const options = {
+      method: 'GET',
+      url: '/students',
+      params: {
+        ...params
+      }
+    };
+
+    return API.request(options);
+  },
+
+  follow: ( userid ) => {
+    const options = {
+      method: 'POST',
+      url: '/follow',
+      data: {
+        user_id: userid
+      }
+    };
+
+    return API.request(options);
+  },
+   
+  unfollow: ( userid ) => {
+    const options = {
+      method: 'POST',
+      url: '/unfollow',
+      data: {
+        user_id: userid
+      }
+    };
+
+    return API.request(options);
+  },
+};
+
+export default StudentsApi;
