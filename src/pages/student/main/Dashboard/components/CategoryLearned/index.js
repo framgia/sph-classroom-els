5f9e5e5ab988ada2f8d26b16bdcb4949152b4060
ErickDoroy <89514595+ErickDoroy@users.noreply.github.com>
2021-12-06 13:28:18 +0800

    E_CLASSROOM-168 [FE] [Student] [Integration] Categories Learned Functionality

--- src/pages/student/main/Dashboard/components/CategoryLearned/index.js
+++ src/pages/student/main/Dashboard/components/CategoryLearned/index.js
@@ -1,74 +1,23 @@
 import React from 'react';
 import Card from 'react-bootstrap/Card';
 import { BiBookAlt } from 'react-icons/bi';
+import { PropTypes } from 'prop-types';
 
 import style from './index.module.css';
 
-const CategoryLearned = () => {
+const CategoryLearned = ({categorylearned}) => {
   return (
-    <Card className={style.bg}>
-      <Card.Header className={style.forContainerBar2}>
-        <p className={style.titleText}>
-                Categories Learned
-        </p>
-        <button className={style.btnVeiw}>View all</button>
-      </Card.Header >
-      <Card.Body>
-        <div className={`${style.forContent_box} ${style.forScroll}`}>
-          <table style={{width: '100%'}}>
-            <tr>
-              <td className={style.listTable}><BiBookAlt size="20px" style={{margin: '0px 17px 5px 0px'}}/>HTML</td>
-              <td className={style.forSeccolum}>5 out of 10 Quizzes Taken</td>
-            </tr>
-            <tr>
-              <td className={style.listTable}><BiBookAlt size="20px" style={{margin: '0px 17px 5px 0px'}}/>JavaScript</td>
-              <td className={style.forSeccolum}>5 out of 10 Quizzes Taken</td>
-            </tr>
-            <tr>
-              <td className={style.listTable}><BiBookAlt size="20px" style={{margin: '0px 17px 5px 0px'}}/>Algebra</td>
-              <td className={style.forSeccolum}>5 out of 10 Quizzes Taken</td>
-            </tr>
-            <tr>
-              <td className={style.listTable}><BiBookAlt size="20px" style={{margin: '0px 17px 5px 0px'}}/>Algebra</td>
-              <td className={style.forSeccolum}>5 out of 10 Quizzes Taken</td>
-            </tr>
-            <tr>
-              <td className={style.listTable}><BiBookAlt size="20px" style={{margin: '0px 17px 5px 0px'}}/>Science</td>
-              <td className={style.forSeccolum}>5 out of 10 Quizzes Taken</td>
-            </tr>
-            <tr>
-              <td className={style.listTable}><BiBookAlt size="20px" style={{margin: '0px 17px 5px 0px'}}/>Mathematics</td>
-              <td className={style.forSeccolum}>5 out of 10 Quizzes Taken</td>
-            </tr>
-            <tr>
-              <tb className={style.listTable}><BiBookAlt size="20px" style={{margin: '0px 17px 5px 0px'}}/>English</tb>
-              <td className={style.forSeccolum}>5 out of 10 Quizzes Taken</td>
-            </tr>
-            <tr>
-              <tb className={style.listTable}><BiBookAlt size="20px" style={{margin: '0px 17px 5px 0px'}}/>English</tb>
-              <td className={style.forSeccolum}>5 out of 10 Quizzes Taken</td>
-            </tr>
-            <tr>
-              <tb className={style.listTable}><BiBookAlt size="20px" style={{margin: '0px 17px 5px 0px'}}/>English</tb>
-              <td className={style.forSeccolum}>5 out of 10 Quizzes Taken</td>
-            </tr>
-            <tr>
-              <tb className={style.listTable}><BiBookAlt size="20px" style={{margin: '0px 17px 5px 0px'}}/>English</tb>
-              <td className={style.forSeccolum}>5 out of 10 Quizzes Taken</td>
-            </tr>
-            <tr>
-              <tb className={style.listTable}><BiBookAlt size="20px" style={{margin: '0px 17px 5px 0px'}}/>English</tb>
-              <td className={style.forSeccolum}>5 out of 10 Quizzes Taken</td>
-            </tr>
-            <tr>
-              <tb className={style.listTable}><BiBookAlt size="20px" style={{margin: '0px 17px 5px 0px'}}/>English</tb>
-              <td className={style.forSeccolum}>5 out of 10 Quizzes Taken</td>
-            </tr>
-          </table>
-        </div>  
-      </Card.Body>
-    </Card>
+    <table style={{width: '100%'}}>
+      <tr>
+        <td className={style.listTable}><BiBookAlt size="20px" style={{margin: '0px 17px 5px 0px'}}/>{categorylearned.name}</td>
+        <td className={style.forSeccolum}>{categorylearned.quizzes.length} out of {categorylearned.quizzes_count} Quizzes Taken</td>
+      </tr>
+    </table>
   );
+};
+
+CategoryLearned.propTypes = {
+  categorylearned: PropTypes.object
 };
 
 export default CategoryLearned;
--- src/pages/student/main/Dashboard/index.js
+++ src/pages/student/main/Dashboard/index.js
@@ -1,12 +1,47 @@
-import React from 'react';
+import { React, useState, useEffect} from 'react';
+import Cookies from 'js-cookie';
 
+import Card from 'react-bootstrap/Card';
 import style from './index.module.css';
 import Recent from './components/Recent';
 import CategoryLearned from './components/CategoryLearned';
 import FriendsActivities from './components/FriendsActivities';
+import DashboardApi from '../../../../api/Dashboard';
 
 
-const Dashboard = () => {
+function Dashboard() {
+  const [categorieslearned, setCategoriesLearned] = useState(null);
+  const  userId = Cookies.get('user_id');
+
+  useEffect(() => {
+    DashboardApi.getAll(userId).then(({ data }) => {
+      setCategoriesLearned(data.data);
+      console.log(data.data);
+    });
+  });
+
+  const renderDashList = () => {
+    return (
+      <Card>
+        <Card.Header className={style.forContainerBar2}>
+          <p className={style.titleText}>
+                  Categories Learned
+          </p>
+          <button className={style.btnVeiw}>View all</button>
+        </Card.Header >
+        <Card.Body>
+          <div className={`${style.forContent_box} ${style.forScroll}`}>
+            { categorieslearned?.map((categorylearned, idx) => {
+              return(
+                <CategoryLearned key={idx} categorylearned={categorylearned}/>
+                  );
+              })}
+          </div> 
+        </Card.Body>
+      </Card>
+    );
+  };
+
   return (
     <div style={{padding: '0px 196px'}}>
       <h2 className={style.h2_style}>Recent</h2>
@@ -17,7 +52,7 @@ const Dashboard = () => {
         <Recent title='CSS'/>
       </div>
       <div className={style.bg2}>
-        <CategoryLearned />
+        {renderDashList()}
         <FriendsActivities />
       </div>
     </div>
