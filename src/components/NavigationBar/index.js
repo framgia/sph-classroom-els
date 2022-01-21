dfe31ad3e7fad9996fe5d20019b55238e4ecdb0a
Mary Therese S. Alegre <marytherese.alegre@sun-asterisk.com>
2022-01-21 14:27:49 +0800

    E_CLASSROOM-219 [Bugfix] Navbar Dropdown Menu Shrinked

--- src/components/NavigationBar/index.js
+++ src/components/NavigationBar/index.js
@@ -23,71 +23,71 @@ const NavigationBar = () => {
   };
 
   return (
-    <Navbar expand="lg" className={style.navbar}>
+    <Navbar expand='lg' className={style.navbar}>
       <Container fluid>
-        <LinkContainer to="/">
-          <Navbar.Brand href="#" className={style.title}>
+        <LinkContainer to='/'>
+          <Navbar.Brand href='#' className={style.title}>
             E-LEARNING
           </Navbar.Brand>
         </LinkContainer>
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
-        <Navbar.Collapse className="justify-content-end">
-          <Nav className="ml-auto">
-            <LinkContainer to="/">
-              <Nav.Link href="#" className={style.navbarLink}>
+        <Navbar.Toggle aria-controls='basic-navbar-nav' />
+        <Navbar.Collapse className='justify-content-end'>
+          <Nav className='ml-auto'>
+            <LinkContainer to='/'>
+              <Nav.Link href='#' className={style.navbarLink}>
                 Dashboard
               </Nav.Link>
             </LinkContainer>
-            <LinkContainer to="/categories">
-              <Nav.Link href="#" className={style.navbarLink}
-              >
+            <LinkContainer to='/categories'>
+              <Nav.Link href='#' className={style.navbarLink}>
                 Categories
               </Nav.Link>
             </LinkContainer>
-            <LinkContainer to="/students">
-              <Nav.Link href="#" className={style.navbarLink}>
+            <LinkContainer to='/students'>
+              <Nav.Link href='#' className={style.navbarLink}>
                 Students
               </Nav.Link>
             </LinkContainer>
             <NavDropdown
               title={
-                <center><img
-                  src="https://www.freeiconspng.com/thumbs/profile-icon-png/account-profile-user-icon--icon-search-engine-10.png"
-                  className={style.dropdownTitleIcon}
-                  alt="Profile Icon"
-                /></center>
+                <center>
+                  <img
+                    src='https://www.freeiconspng.com/thumbs/profile-icon-png/account-profile-user-icon--icon-search-engine-10.png'
+                    className={style.dropdownTitleIcon}
+                    alt='Profile Icon'
+                  />
+                </center>
               }
               id={style.dropdownMenu}
-              align="end"
+              align='end'
             >
-              <LinkContainer 
-                exact to="/learnings" 
+              <LinkContainer
+                exact
+                to='/learnings'
                 activeClassName={style.colorActive}
               >
-                <NavDropdown.Item 
-                  href="#" 
-                  className={style.dropdownItem}
-                >
-                  <IoLibraryOutline /> Learnings
+                <NavDropdown.Item href='#' className={style.dropdownItem}>
+                  <IoLibraryOutline />{' '}
+                  <span className={style.dropdownItemName}>Learnings</span>
                 </NavDropdown.Item>
               </LinkContainer>
-              <LinkContainer 
-                exact to="/profile"
+              <LinkContainer
+                exact
+                to='/profile'
                 activeClassName={style.colorActive}
               >
-                <NavDropdown.Item 
-                  href="#" 
-                  className={style.dropdownItem}
-                >
-                  <CgProfile /> Profile
+                <NavDropdown.Item href='#' className={style.dropdownItem}>
+                  <CgProfile />{' '}
+                  <span className={style.dropdownItemName}>Profile</span>
                 </NavDropdown.Item>
               </LinkContainer>
               <NavDropdown.Item
-                href="#"
+                href='#'
                 className={style.dropdownItem}
                 onClick={onLogout}
               >
-                <BiLogOutCircle /> Logout
+                <BiLogOutCircle />{' '}
+                <span className={style.dropdownItemName}>Logout</span>
               </NavDropdown.Item>
             </NavDropdown>
           </Nav>
