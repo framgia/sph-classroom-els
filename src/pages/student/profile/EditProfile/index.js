a45c87b0bd882d4cf12e1c1fd153d1dd0340db61
Joash Cañete <joash.canete@sun-asterisk.com>
2022-01-28 12:07:04 +0800

    E_CLASSROOM-215-FE/BE-Profile-Edit-Function-spinner

--- src/pages/student/profile/EditProfile/index.js
+++ src/pages/student/profile/EditProfile/index.js
@@ -9,6 +9,7 @@ import { Alert } from 'react-bootstrap';
 import { Controller } from 'react-hook-form';
 import { useEffect } from 'react';
 import { PropTypes } from 'prop-types';
+import Spinner from 'react-bootstrap/Spinner';
 
 import ProfileEditApi from '../../../../api/ProfileEdit';
 import StudentsApi from '../../../../api/Student';
@@ -73,125 +74,132 @@ const EditProfile = () => {
         }}
       >
         <div className={style.HeadingText}>Edit Account Info</div>
-        <Form
-          onSubmit={handleSubmit(handleOnSubmit)}
-          style={{ marginTop: '20px' }}
-        >
-          <Form.Group className="mb-3" controlId="formBasicEmail">
-            <Form.Label
-              style={{
-                marginBottom: '0px',
-                color: '#48535B',
-                fontSize: '16px',
-              }}
-            >
-              Name
-            </Form.Label>
-            {profileName ? (
-              <Controller
-                control={control}
-                name="name"
-                defaultValue={profileName?.name}
-                render={({ field: { onChange, value, ref } }) => (
-                  <Form.Control
-                    onChange={onChange}
-                    value={value}
-                    ref={ref}
-                    className="cntrs"
-                    type="text"
-                    placeholder="e.g. jhondoe"
-                    isInvalid={!!errors?.name}
-                    required
-                    maxLength={50}
-                  />
-                )}
-              />
-            ) : (
-              ''
-            )}
-            <Form.Control.Feedback type="invalid">
-              {errors?.name}
-            </Form.Control.Feedback>
-          </Form.Group>
-
-          <Form.Group className="mb-4" controlId="formBasicPassword">
-            <Form.Label style={{ marginBottom: '0px', color: '#48535B' }}>
-              Email
-            </Form.Label>
-            {profileName ? (
-              <Controller
-                control={control}
-                name="email"
-                defaultValue={profileName?.email}
-                render={({ field: { onChange, value, ref } }) => (
-                  <Form.Control
-                    onChange={onChange}
-                    value={value}
-                    ref={ref}
-                    className="cntrs"
-                    type="email "
-                    placeholder="e.g. jhondoe@gmail.com"
-                    isInvalid={!!errors?.email}
-                    required
-                    maxLength={50}
-                  />
-                )}
-              />
-            ) : (
-              ''
-            )}
-
-            <Form.Control.Feedback type="invalid">
-              {errors?.email}
-            </Form.Control.Feedback>
-          </Form.Group>
-
-          <Form.Group className="mb-4" controlId="formBasicPassword">
-            <Form.Label style={{ marginBottom: '0px', color: '#48535B' }}>
-              Password
-            </Form.Label>
-            {profileName ? (
-              <Controller
-                control={control}
-                name="password"
-                defaultValue={profileName?.password}
-                render={({ field: { onChange, value, ref } }) => (
-                  <Form.Control
-                    onChange={onChange}
-                    value={value}
-                    ref={ref}
-                    className="cntrs"
-                    type="password"
-                    placeholder="**********"
-                    isInvalid={!!errors?.password}
-                    required
-                    maxLength={50}
-                  />
-                )}
-              />
-            ) : (
-              ''
-            )}
-            <Form.Control.Feedback type="invalid">
-              {errors?.password}
-            </Form.Control.Feedback>
-          </Form.Group>
-
-          <div className={style.buttonposition}>
-            <Button
-              className={style.changepassbutton}
-              variant="primary"
-              type="submit"
-            >
-              Change
-            </Button>
-            <div>
-              <a className={style.cancel} href="/profile">
-                Cancel
-              </a>
-            </div>
+        {profileName === null ? (
+          <div className={style.loading}>
+            <Spinner animation="border" role="status"></Spinner>
+            <span className={style.loadingWord}>Loading</span>
           </div>
-        </Form>
+        ) : (
+          <Form
+            onSubmit={handleSubmit(handleOnSubmit)}
+            style={{ marginTop: '20px' }}
+          >
+            <Form.Group className="mb-3" controlId="formBasicEmail">
+              <Form.Label
+                style={{
+                  marginBottom: '0px',
+                  color: '#48535B',
+                  fontSize: '16px',
+                }}
+              >
+                Name
+              </Form.Label>
+              {profileName ? (
+                <Controller
+                  control={control}
+                  name="name"
+                  defaultValue={profileName?.name}
+                  render={({ field: { onChange, value, ref } }) => (
+                    <Form.Control
+                      onChange={onChange}
+                      value={value}
+                      ref={ref}
+                      className="cntrs"
+                      type="text"
+                      placeholder="e.g. jhondoe"
+                      isInvalid={!!errors?.name}
+                      required
+                      maxLength={50}
+                    />
+                  )}
+                />
+              ) : (
+                ''
+              )}
+              <Form.Control.Feedback type="invalid">
+                {errors?.name}
+              </Form.Control.Feedback>
+            </Form.Group>
+
+            <Form.Group className="mb-4" controlId="formBasicPassword">
+              <Form.Label style={{ marginBottom: '0px', color: '#48535B' }}>
+                Email
+              </Form.Label>
+              {profileName ? (
+                <Controller
+                  control={control}
+                  name="email"
+                  defaultValue={profileName?.email}
+                  render={({ field: { onChange, value, ref } }) => (
+                    <Form.Control
+                      onChange={onChange}
+                      value={value}
+                      ref={ref}
+                      className="cntrs"
+                      type="email"
+                      placeholder="e.g. jhondoe@gmail.com"
+                      isInvalid={!!errors?.email}
+                      required
+                      maxLength={50}
+                    />
+                  )}
+                />
+              ) : (
+                ''
+              )}
+
+              <Form.Control.Feedback type="invalid">
+                {errors?.email}
+              </Form.Control.Feedback>
+            </Form.Group>
+
+            <Form.Group className="mb-4" controlId="formBasicPassword">
+              <Form.Label style={{ marginBottom: '0px', color: '#48535B' }}>
+                Password
+              </Form.Label>
+              {profileName ? (
+                <Controller
+                  control={control}
+                  name="password"
+                  defaultValue={profileName?.password}
+                  render={({ field: { onChange, value, ref } }) => (
+                    <Form.Control
+                      onChange={onChange}
+                      value={value}
+                      ref={ref}
+                      className="cntrs"
+                      type="password"
+                      placeholder="**********"
+                      isInvalid={!!errors?.password}
+                      required
+                      maxLength={50}
+                    />
+                  )}
+                />
+              ) : (
+                ''
+              )}
+              <Form.Control.Feedback type="invalid">
+                {errors?.password}
+              </Form.Control.Feedback>
+            </Form.Group>
+
+            <div className={style.buttonposition}>
+              <Button
+                className={style.changepassbutton}
+                variant="primary"
+                type="submit"
+              >
+                Change
+              </Button>
+              <div>
+                <a className={style.cancel} href="/profile">
+                  Cancel
+                </a>
+              </div>
+            </div>
+          </Form>
+        )}
       </Card>
     </div>
   );
