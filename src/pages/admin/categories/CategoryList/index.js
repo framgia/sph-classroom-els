6aa1b306a148a18a598bb93139b6d1756ebf7061
Mary Therese S. Alegre <marytherese.alegre@sun-asterisk.com>
2022-02-03 16:15:29 +0800

    E_CLASSROOM-234 [Admin] [FE/BE] Dynamic Data for Displaying Categories List

--- src/pages/admin/categories/CategoryList/index.js
+++ src/pages/admin/categories/CategoryList/index.js
@@ -1,4 +1,5 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { useHistory } from 'react-router-dom';
 import Card from 'react-bootstrap/Card';
 import { Col, Table } from 'react-bootstrap';
 import Form from 'react-bootstrap/Form';
@@ -13,30 +14,66 @@ import Pagination from '../../../../components/Pagination';
 
 import style from './index.module.scss';
 
+import CategoryApi from '../../../../api/Category';
+
 const CategoryList = () => {
+  const queryParams = new URLSearchParams(window.location.search);
+  const pageNum = queryParams.get('page');
 
-  const categories = [
-    {name: 'Programming', id: '1'}
-    ,{name: 'Science', id: '2'}
-    ,{name: 'Mathematics', id: '3'}
-    ,{name: 'Web Development', id: '4'}
-    ,{name: 'Data Structures', id: '5'}
-    ,{name: 'History', id: '6'}
-    ,{name: 'Technology', id: '7'}
-    ,{name: 'Sports', id: '8'}
-    ,{name: 'Computer Networking', id: '9'}
-    ,{name: 'Philosophy', id: '10'}
-    ,{name: 'C Programming', id: '11'}
-  ];
+  //TEMPORARY VALUES since the search, sort, and filter functionality isn't implemented yet.
+  const sortBy = 'asc';
+  const filter = '';
+  const search = '';
 
-  const renderList = () =>{
-    return categories.map((category, idx) => {
+  const history = useHistory();
+
+  const [categories, setCategories] = useState(null);
+  const [page, setPage] = useState(pageNum ? parseInt(pageNum) : 1);
+  const [perPage, setPerPage] = useState(0);
+  const [totalItems, setTotalItems] = useState(0);
+  const [lastPage, setLastPage] = useState(0);
+
+  useEffect(() => {
+    history.push(`?page=${page}`);
+
+    CategoryApi.getAll({
+      page: page,
+      sortBy: sortBy,
+      filter: filter,
+      search: search
+    }).then(({ data }) => {
+      setCategories(data.data);
+      setPerPage(data.per_page);
+      setTotalItems(data.total);
+      setLastPage(data.last_page);
+    });
+  }, [page]);
+
+  const onPageChange = (selected) => {
+    setPage(selected + 1);
+
+    history.push(`?page=${selected + 1}`);
+  };
+
+  const renderList = () => {
+    return categories?.map((category, idx) => {
       return (
         <tr key={idx}>
           <td id={style.tBodyStyle}>{category.id}</td>
           <td id={style.tBodyStyle}>{category.name}</td>
-          <td id={style.tBodyStyle1}><Button className={style.designButton}><FaRegEdit size="20px"/></Button></td>
-          <td id={style.tBodyStyle1}><Button className={style.designButton}><RiDeleteBin2Fill size="30px" color="#db7771"/></Button></td>
+          <td id={style.tBodyStyle} className={`${style.paragraphEllipsis}`}>
+            {category.description}
+          </td>
+          <td id={style.tBodyStyle1}>
+            <Button className={style.designButton}>
+              <FaRegEdit size="20px" />
+            </Button>
+          </td>
+          <td id={style.tBodyStyle1}>
+            <Button className={style.designButton}>
+              <RiDeleteBin2Fill size="30px" color="#db7771" />
+            </Button>
+          </td>
         </tr>
       );
     });
@@ -68,9 +105,7 @@ const CategoryList = () => {
                     variant="link"
                     bsPrefix="none"
                   >
-                    <span className={style.dropdownText}>
-              Filter
-                    </span>
+                    <span className={style.dropdownText}>Filter</span>
                     <VscFilter size="20px" />
                   </Dropdown.Toggle>
                 </Dropdown>
@@ -85,29 +120,34 @@ const CategoryList = () => {
               </table>
             </Card.Header>
             <Card.Body className={style.cardBodyScroll}>
-              <Button className={style.button}>
-                    Add Category
-              </Button>
+              <Button className={style.button}>Add Category</Button>
               <div>
                 <Table className={style.formatTable}>
                   <thead>
                     <tr>
                       <td className={style.firstCol}>ID</td>
                       <td className={style.firstCol}>Name</td>
+                      <td className={style.firstCol}>Description</td>
                       <td className={style.firstCol1}>Edit</td>
                       <td className={style.firstCol1}>Delete</td>
                     </tr>
                   </thead>
-                  <tbody>
-                    {renderList()}
-                  </tbody>
+                  <tbody>{renderList()}</tbody>
                 </Table>
               </div>
             </Card.Body>
           </Card>
         </Col>
         <div className="pt-4">
-          <p id={style.paginateStyle}>1 - 24 of 36</p><div id={style.paginateStyle}><Pagination></Pagination></div>
+          <div id={style.paginateStyle}>
+            <Pagination
+              page={page}
+              perPage={perPage}
+              totalItems={totalItems}
+              pageCount={lastPage}
+              onPageChange={onPageChange}
+            ></Pagination>
+          </div>
         </div>
       </div>
     </div>
