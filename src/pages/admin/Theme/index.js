399f4ca4e8e746cf4bcd1368744891e387682569
Mary Therese S. Alegre <marytherese.alegre@sun-asterisk.com>
2022-03-14 16:03:59 +0800

    E_CLASSROOM-306 [FE] Reusable Dropdown Component

--- src/pages/admin/Theme/index.js
+++ src/pages/admin/Theme/index.js
@@ -1,7 +1,46 @@
-import React from 'react';
+import React, { useState } from 'react';
+
+import FilterDropdown from '../../../components/FilterDropdown';
 
 const ThemePage = () => {
-  return <div className="container"></div>;
+  const [filter, setFilter] = useState('');
+  const dropdownOptions = [
+    { name: 'Option 1' },
+    { name: 'Option 2' },
+    { name: 'Option 3' },
+    { name: 'Option 4' },
+    { name: 'Option 5' },
+    { name: 'Option 6' },
+    { name: 'Option 7' },
+    { name: 'Option 8' },
+    { name: 'Option 9' },
+    { name: 'Option 10' }
+  ];
+
+  return (
+    <div className="container mt-5 d-flex-column gap-5">
+      <div>
+        <h3 className="mb-4">
+          <u>Dropdown Menu</u>
+        </h3>
+        <div className="d-flex gap-5">
+          <FilterDropdown
+            dropdownLabel="Scrollable Dropdown"
+            dropdownItems={dropdownOptions}
+            isScrollable={true}
+            filter={filter}
+            setFilter={setFilter}
+          />
+          <FilterDropdown
+            dropdownLabel="Fixed Dropdown"
+            dropdownItems={dropdownOptions}
+            filter={filter}
+            setFilter={setFilter}
+          />
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default ThemePage;
