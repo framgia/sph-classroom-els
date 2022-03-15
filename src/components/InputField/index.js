678a3d92f52c2bd5b9d0a86603cdda08db210900
ErickDoroy <89514595+ErickDoroy@users.noreply.github.com>
2022-03-15 14:44:42 +0800

    E_CLASSROOM-305 update the instruction to use this component

--- src/components/InputField/index.js
+++ src/components/InputField/index.js
@@ -5,25 +5,34 @@ import { PropTypes } from 'prop-types';
 /*
   To use this component, pass these props:
 
-  > ref          : Pass any value
+  > as           : The underlying HTML element to use when rendering the FormControl.
+                   Possible value are "input" or "textarea"
+
+  > ref          : Pass any value 
 
   > type         : Pass a string value that can be used to identify the input field.
+                    Posible value are "text", "email" or "password"
 
   > value        : Pass a string value input
+                   Possible input is what the user put or write in the input field
 
-  > onChange     : Pass a string value.
+  > onChange     : Pass a function value.
 
-  > isInvalid    : Pass the error
+  > isInvalid    : You need to pass the error, you can test this by using boolean
 
   > fieldSize    : Pass the size's of the input field by lg, md, and sm
+                   Possible value are "lg", "md" or "sm"
+
 
   > placeholder  : Pass a string value for the placeholder
+                   Possible value sentence for EX. "Category Name"
 
 */
 
 import style from './index.module.scss';
 
 const InputField = ({
+  as,
   ref,
   type,
   value,
@@ -48,6 +57,7 @@ const InputField = ({
 
   return (
     <Form.Control
+      as={as}
       ref={ref}
       type={type}
       value={value}
@@ -60,10 +70,11 @@ const InputField = ({
 };
 
 InputField.propTypes = {
+  as: PropTypes.string,
   ref: PropTypes.any,
   type: PropTypes.string,
   value: PropTypes.string,
-  onChange: PropTypes.string,
+  onChange: PropTypes.func,
   isInvalid: PropTypes.bool,
   fieldSize: PropTypes.string,
   placeholder: PropTypes.string
