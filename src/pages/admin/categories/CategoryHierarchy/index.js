76be0471968cd439cdc8f6b75b68962c0191e237
Joash Cañete <joash.canete@sun-asterisk.com>
2022-03-29 16:14:01 +0800

    E_CLASSROOM-375 - category Hierarchy has changed to Save

--- src/pages/admin/categories/CategoryHierarchy/index.js
+++ src/pages/admin/categories/CategoryHierarchy/index.js
@@ -237,6 +237,7 @@ const CategoryHierarchy = () => {
         isSaved={isSaved}
         setIsSaved={setIsSaved}
         modalHeaderTitle="Move to..."
+        buttonType="Save"
       />
     </div>
   );
