c3e754fc3b79f05ecf1abafceeb9312fd81e282e
ErickDoroy <89514595+ErickDoroy@users.noreply.github.com>
2022-03-30 11:58:57 +0800

    E_CLASSROOM-342 [FE/BE] Implement Change Password for Student Profile

--- src/pages/student/profile/ChangePassword/index.js
+++ src/pages/student/profile/ChangePassword/index.js
@@ -1,18 +1,63 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useForm, Controller } from 'react-hook-form';
-import { Link } from 'react-router-dom';
-import Button from '../../../../components/Button';
-import InputField from '../../../../components/InputField';
+import { Link, useHistory } from 'react-router-dom';
+import { useToast } from '../../../../hooks/useToast';
 import Container from 'react-bootstrap/Container';
 import Form from 'react-bootstrap/Form';
+
 import style from './index.module.scss';
+import AdminApi from '../../../../api/Admin';
+import Button from '../../../../components/Button';
+import InputField from '../../../../components/InputField';
 
 const UsersEdit = () => {
-  const { control } = useForm();
+  const { control, handleSubmit, reset } = useForm();
+  const [errors, setErrors] = useState({});
+  const [submitStatus, setSubmitStatus] = useState(false);
+
+  const history = useHistory();
+  const toast = useToast();
+
+  const handleOnSubmit = async ({
+    password,
+    new_password,
+    password_confirmation
+  }) => {
+    setSubmitStatus(true);
+    toast('Processing', 'Updating your current password...');
+    setErrors({});
+
+    await AdminApi.updatePassword({
+      password,
+      new_password,
+      password_confirmation
+    })
+      .then(({ data }) => {
+        history.push('/profile/view');
+        toast('Success', data.message);
+        setSubmitStatus(false);
+        reset({
+          password: '',
+          new_password: '',
+          password_confirmation: ''
+        });
+      })
+      .catch((error) => {
+        setSubmitStatus(false);
+        toast(
+          'Error',
+          error?.response?.data?.message ||
+            error?.response?.data?.error?.password
+        );
+        setErrors(
+          error?.response?.data?.errors || error?.response?.data?.error
+        );
+      });
+  };
 
   return (
     <Container className={style.formContainer}>
-      <Form className={style.card}>
+      <Form className={style.card} onSubmit={handleSubmit(handleOnSubmit)}>
         <div className={style.headingText}>Change Password</div>
         <Form.Group className="mb-4 mt-4" controlId="currentPassword">
           <Form.Label className={style.inputLabels}>
@@ -20,8 +65,7 @@ const UsersEdit = () => {
           </Form.Label>
           <Controller
             control={control}
-            name="current_password"
-            defaultValue=""
+            name="password"
             render={({ field: { onChange, value, ref } }) => (
               <InputField
                 ref={ref}
@@ -30,12 +74,14 @@ const UsersEdit = () => {
                 fieldSize="md"
                 maxLength={20}
                 onChange={onChange}
-                isInvalid=""
+                isInvalid={!!errors?.password}
                 placeholder="Enter current password here..."
               />
             )}
           />
-          <Form.Control.Feedback type="invalid"></Form.Control.Feedback>
+          <Form.Control.Feedback type="invalid">
+            {errors?.password}
+          </Form.Control.Feedback>
         </Form.Group>
 
         <Form.Group className="mb-4" controlId="newPassword">
@@ -43,7 +89,6 @@ const UsersEdit = () => {
           <Controller
             control={control}
             name="new_password"
-            defaultValue=""
             render={({ field: { onChange, value, ref } }) => (
               <InputField
                 ref={ref}
@@ -52,12 +97,14 @@ const UsersEdit = () => {
                 fieldSize="md"
                 maxLength={20}
                 onChange={onChange}
-                isInvalid=""
-                placeholder="min. of 6 characters"
+                isInvalid={!!errors?.new_password}
+                placeholder="Min. of 6 characters"
               />
             )}
           />
-          <Form.Control.Feedback type="invalid"></Form.Control.Feedback>
+          <Form.Control.Feedback type="invalid">
+            {errors?.new_password}
+          </Form.Control.Feedback>
         </Form.Group>
 
         <Form.Group className="mb-4" controlId="confirmPassword">
@@ -66,8 +113,7 @@ const UsersEdit = () => {
           </Form.Label>
           <Controller
             control={control}
-            name="confirm_password"
-            defaultValue=""
+            name="password_confirmation"
             render={({ field: { onChange, value, ref } }) => (
               <InputField
                 ref={ref}
@@ -76,12 +122,14 @@ const UsersEdit = () => {
                 fieldSize="md"
                 maxLength={20}
                 onChange={onChange}
-                isInvalid=""
+                isInvalid={!!errors?.password_confirmation}
                 placeholder="Match with new password"
               />
             )}
           />
-          <Form.Control.Feedback type="invalid"></Form.Control.Feedback>
+          <Form.Control.Feedback style={{ width: '300px' }} type="invalid">
+            {errors?.password_confirmation}
+          </Form.Control.Feedback>
         </Form.Group>
 
         <div className={style.formButtons}>
@@ -94,7 +142,7 @@ const UsersEdit = () => {
             buttonLabel="Change"
             buttonSize="sm"
             type="submit"
-            disabled=""
+            disabled={submitStatus}
           />
         </div>
       </Form>
